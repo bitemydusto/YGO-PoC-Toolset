bd52680c36cc9cfb108e1a5f912bd1023a9a2ee4
bitemydusto <bitemydusto@users.noreply.github.com>
2026-07-27 15:13:14 +0200

    Add index comments to Recipes2 and Recipes3 arrays

--- src/Mods/FusionRequirements/DllMain.cpp
+++ src/Mods/FusionRequirements/DllMain.cpp
@@ -38,262 +38,262 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID)
 
 constexpr Fusion2 Recipes2[] =
 {
-	Cards::FLAME_SWORDSMAN,
+	Cards::FLAME_SWORDSMAN, // Index: [0]
 	{
 		Cards::FLAME_MANIPULATOR,
 		Cards::MASAKI_THE_LEGENDARY_SWORDSMAN
 	},
-	Cards::ZOMBIE_WARRIOR,
+	Cards::ZOMBIE_WARRIOR, // Index: [1]
 	{
 		Cards::SKULL_SERVANT,
 		Cards::BATTLE_WARRIOR
 	},
-	Cards::GAIA_THE_DRAGON_CHAMPION,
+	Cards::GAIA_THE_DRAGON_CHAMPION, // Index: [2]
 	{
 		Cards::GAIA_THE_FIERCE_KNIGHT,
 		Cards::CURSE_OF_DRAGON
 	},
-	Cards::KARBONALA_WARRIOR,
+	Cards::KARBONALA_WARRIOR, // Index: [3]
 	{
 		Cards::M_WARRIOR_No1,
 		Cards::M_WARRIOR_No2
 	},
-	Cards::THOUSAND_DRAGON,
+	Cards::THOUSAND_DRAGON, // Index: [4]
 	{
 		Cards::BABY_DRAGON,
 		Cards::TIME_WIZARD
 	},
-	Cards::BAROX,
+	Cards::BAROX, // Index: [5]
 	{
 		Cards::RYU_KISHIN,
 		Cards::FRENZIED_PANDA
 	},
-	Cards::RABID_HORSEMAN,
+	Cards::RABID_HORSEMAN, // Index: [6]
 	{
 		Cards::BATTLE_OX,
 		Cards::MYSTIC_HORSEMAN
 	},
-	Cards::KAMIONWIZARD,
+	Cards::KAMIONWIZARD, // Index: [7]
 	{
 		Cards::MYSTICAL_ELF,
 		Cards::CURTAIN_OF_THE_DARK_ONES
 	},
-	Cards::CHARUBIN_THE_FIRE_KNIGHT,
+	Cards::CHARUBIN_THE_FIRE_KNIGHT, // Index: [8]
 	{
 		Cards::MONSTER_EGG,
 		Cards::HINOTAMA_SOUL
 	},
-	Cards::DARKFIRE_DRAGON,
+	Cards::DARKFIRE_DRAGON, // Index: [9]
 	{
 		Cards::FIREGRASS,
 		Cards::PETIT_DRAGON
 	},
-	Cards::FUSIONIST,
+	Cards::FUSIONIST, // Index: [10]
 	{
 		Cards::PETIT_ANGEL,
 		Cards::MYSTICAL_SHEEP_No2
 	},
-	Cards::FLAME_GHOST,
+	Cards::FLAME_GHOST, // Index: [11]
 	{
 		Cards::SKULL_SERVANT,
 		Cards::DISSOLVEROCK
 	},
-	Cards::B_SKULL_DRAGON,
+	Cards::B_SKULL_DRAGON, // Index: [12]
 	{
 		Cards::SUMMONED_SKULL,
 		Cards::RED_EYES_B_DRAGON
 	},
-	Cards::ROARING_OCEAN_SNAKE,
+	Cards::ROARING_OCEAN_SNAKE, // Index: [13]
 	{
 		Cards::MYSTIC_LAMP,
 		Cards::HYOSUBE
 	},
-	Cards::RARE_FISH,
+	Cards::RARE_FISH, // Index: [14]
 	{
 		Cards::FUSIONIST,
 		Cards::ENCHANTING_MERMAID
 	},
-	Cards::MAVELUS,
+	Cards::MAVELUS, // Index: [15]
 	{
 		Cards::TYHONE,
 		Cards::WINGS_OF_WICKED_FLAME
 	},
-	Cards::DRAGONESS_THE_WICKED_KNIGHT,
+	Cards::DRAGONESS_THE_WICKED_KNIGHT, // Index: [16]
 	{
 		Cards::ARMAILL,
 		Cards::ONE_EYED_SHIELD_DRAGON
 	},
-	Cards::LABYRINTH_TANK,
+	Cards::LABYRINTH_TANK, // Index: [17]
 	{
 		Cards::GIGA_TECH_WOLF,
 		Cards::CANNON_SOLDIER
 	},
-	Cards::BICKURIBOX,
+	Cards::BICKURIBOX, // Index: [18]
 	{
 		Cards::CRASS_CLOWN,
 		Cards::DREAM_CLOWN
 	},
-	Cards::GILTIA_THE_D_KNIGHT,
+	Cards::GILTIA_THE_D_KNIGHT, // Index: [19]
 	{
 		Cards::GUARDIAN_OF_THE_LABYRINTH,
 		Cards::PROTECTOR_OF_THE_THRONE
 	},
-	Cards::METAL_DRAGON,
+		Cards::METAL_DRAGON, // Index: [20]
 	{
 		Cards::STEEL_OGRE_GROTTO_No1,
 		Cards::LESSER_DRAGON
 	},
-	Cards::KAISER_DRAGON,
+	Cards::KAISER_DRAGON, // Index: [21]
 	{
 		Cards::WINGED_DRAGON_GUARDIAN_OF_THE_FORTRESS_No1,
 		Cards::FAIRY_DRAGON
 	},
-	Cards::DEEPSEA_SHARK,
+	Cards::DEEPSEA_SHARK, // Index: [22]
 	{
 		Cards::BOTTOM_DWELLER,
 		Cards::TONGYO
 	},
-	Cards::KAMINARI_ATTACK,
+	Cards::KAMINARI_ATTACK, // Index: [23]
 	{
 		Cards::OCUBEAM,
 		Cards::MEGA_THUNDERBALL
 	},
-	Cards::PUNISHED_EAGLE,
+	Cards::PUNISHED_EAGLE, // Index: [24]
 	{
 		Cards::NIWATORI,
 		Cards::BLUE_WINGED_CROWN
 	},
-	Cards::CRIMSON_SUNBIRD,
+	Cards::CRIMSON_SUNBIRD, // Index: [25]
 	{
 		Cards::FAITH_BIRD,
 		Cards::SKULL_RED_BIRD
 	},
-	Cards::SOUL_HUNTER,
+	Cards::SOUL_HUNTER, // Index: [26]
 	{
 		Cards::LORD_OF_THE_LAMP,
 		Cards::INVADER_FROM_ANOTHER_DIMENSION
 	},
-	Cards::VERMILLION_SPARROW,
+	Cards::VERMILLION_SPARROW, // Index: [27]
 	{
 		Cards::RHAIMUNDOS_OF_THE_RED_SWORD,
 		Cards::FIREYAROU
 	},
-	Cards::PRAGTICAL,
+	Cards::PRAGTICAL, // Index: [28]
 	{
 		Cards::TRAKADON,
 		Cards::FLAME_VIPER
 	},
-	Cards::FLOWER_WOLF,
+	Cards::FLOWER_WOLF, // Index: [29]
 	{
 		Cards::SILVER_FANG,
 		Cards::DARKWORLD_THORNS
 	},
-	Cards::MUSICIAN_KING,
+	Cards::MUSICIAN_KING, // Index: [30]
 	{
 		Cards::WITCH_OF_THE_BLACK_FOREST,
 		Cards::LADY_OF_FAITH
 	},
-	Cards::CYBER_SAURUS,
+	Cards::CYBER_SAURUS, // Index: [31]
 	{
 		Cards::TWO_HEADED_KING_REX,
 		Cards::BLAST_JUGGLER
 	},
-	Cards::BRACCHIO_RAIDUS,
+	Cards::BRACCHIO_RAIDUS, // Index: [32]
 	{
 		Cards::TWO_HEADED_KING_REX,
 		Cards::CRAWLING_DRAGON_No2
 	},
-	Cards::SKULLBIRD,
+	Cards::SKULLBIRD, // Index: [33]
 	{
 		Cards::TEMPLE_OF_SKULLS,
 		Cards::TAKUHEE
 	},
-	Cards::MYSTICAL_SAND,
+	Cards::MYSTICAL_SAND, // Index: [34]
 	{
 		Cards::GIANT_SOLDIER_OF_STONE,
 		Cards::ANCIENT_ELF
 	},
-	Cards::KWAGAR_HERCULES,
+	Cards::KWAGAR_HERCULES, // Index: [35]
 	{
 		Cards::HERCULES_BEETLE,
 		Cards::KUWAGATA_ALPHA
 	},
-	Cards::SKELGON,
+	Cards::SKELGON, // Index: [36]
 	{
 		Cards::BLACKLAND_FIRE_DRAGON,
 		Cards::THE_SNAKE_HAIR
 	},
-	Cards::GREAT_MAMMOTH_OF_GOLDFINE,
+	Cards::GREAT_MAMMOTH_OF_GOLDFINE, // Index: [37]
 	{
 		Cards::THE_SNAKE_HAIR,
 		Cards::DRAGON_ZOMBIE
 	},
-	Cards::EMPRESS_JUDGE,
+	Cards::EMPRESS_JUDGE, // Index: [38]
 	{
 		Cards::QUEENS_DOUBLE,
 		Cards::HIBIKIME
 	},
-	Cards::ROSE_SPECTRE_OF_DUNN,
+	Cards::ROSE_SPECTRE_OF_DUNN, // Index: [39]
 	{
 		Cards::FERAL_IMP,
 		Cards::SNAKEYASHI
 	},
-	Cards::TWIN_HEADED_THUNDER_DRAGON,
+	Cards::TWIN_HEADED_THUNDER_DRAGON, // Index: [40]
 	{
 		Cards::THUNDER_DRAGON,
 		Cards::THUNDER_DRAGON
 	},
-	Cards::MARINE_BEAST,
+	Cards::MARINE_BEAST, // Index: [41]
 	{
 		Cards::WATER_MAGICIAN,
 		Cards::BEHEGON
 	},
-	Cards::WARRIOR_OF_TRADITION,
+	Cards::WARRIOR_OF_TRADITION, // Index: [42]
 	{
 		Cards::BEAUTIFUL_HEADHUNTRESS,
 		Cards::SONIC_MAID
 	},
-	Cards::AMPHIBIOUS_BUGROTH,
+	Cards::AMPHIBIOUS_BUGROTH, // Index: [43]
 	{
 		Cards::GROUND_ATTACKER_BUGROTH,
 		Cards::GUARDIAN_OF_THE_SEA
 	},
-	Cards::SKULL_KNIGHT,
+	Cards::SKULL_KNIGHT, // Index: [44]
 	{
 		Cards::TAINTED_WISDOM,
 		Cards::ANCIENT_BRAIN
 	},
-	Cards::METEOR_B_DRAGON,
+	Cards::METEOR_B_DRAGON, // Index: [45]
 	{
 		Cards::RED_EYES_B_DRAGON,
 		Cards::METEOR_DRAGON
 	},
-	Cards::ALLIGATORS_SWORD_DRAGON,
+	Cards::ALLIGATORS_SWORD_DRAGON, // Index: [46]
 	{
 		Cards::BABY_DRAGON,
 		Cards::ALLIGATORS_SWORD
 	},
-	Cards::CHIMERA_THE_FLYING_MYTHICAL_BEAST,
+	Cards::CHIMERA_THE_FLYING_MYTHICAL_BEAST, // Index: [47]
 	{
 		Cards::GAZELLE_THE_KING_OF_MYTHICAL_BEASTS,
 		Cards::BERFOMET
 	},
-	Cards::THOUSAND_EYES_RESTRICT,
+	Cards::THOUSAND_EYES_RESTRICT, // Index: [48]
 	{
 		Cards::RELINQUISHED,
 		Cards::THOUSAND_EYES_IDOL
 	},
-	Cards::HUMANOID_WORM_DRAKE,
+	Cards::HUMANOID_WORM_DRAKE, // Index: [49]
 	{
 		Cards::WORM_DRAKE,
 		Cards::HUMANOID_SLIME
 	},
-	Cards::ST_JOAN,
+	Cards::ST_JOAN, // Index: [50]
 	{
 		Cards::THE_FORGIVING_MAIDEN,
 		Cards::MARIE_THE_FALLEN_ONE
 	},
-	Cards::THE_LAST_WARRIOR_FROM_ANOTHER_PLANET,
+	Cards::THE_LAST_WARRIOR_FROM_ANOTHER_PLANET, // Index: [51]
 	{
 		Cards::ZOMBYRA_THE_DARK,
 		Cards::MARYOKUTAI
@@ -301,19 +301,19 @@ constexpr Fusion2 Recipes2[] =
 };
 constexpr Fusion3 Recipes3[] =
 {
-	Cards::BLUE_EYES_ULTIMATE_DRAGON,
+	Cards::BLUE_EYES_ULTIMATE_DRAGON, // Index: [0]
 	{
 		Cards::BLUE_EYES_WHITE_DRAGON,
 		Cards::BLUE_EYES_WHITE_DRAGON,
 		Cards::BLUE_EYES_WHITE_DRAGON
 	},
-	Cards::AQUA_DRAGON,
+	Cards::AQUA_DRAGON, // Index: [1]
 	{
 		Cards::ZONE_EATER,
 		Cards::FAIRY_DRAGON,
 		Cards::AMAZON_OF_THE_SEAS
 	},
-	Cards::MAN_EATING_BLACK_SHARK,
+	Cards::MAN_EATING_BLACK_SHARK, // Index: [2]
 	{
 		Cards::SEA_KAMEN,
 		Cards::AMAZON_OF_THE_SEAS,

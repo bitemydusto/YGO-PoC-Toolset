2f3a89a4d0a2f89df90298fe470e97d3c5447fe6
bitemydusto <bitemydusto@users.noreply.github.com>
2026-08-01 14:08:27 +0200

    Add Breaker mod project with custom card effect hook
    Implemented DllMain.cpp to hook and modify card 0x96 (Breaker the Magical Warrior), adjusting its stat buff and effect with custom logic and memory manipulation. Ensured effect applies only once per zone.

---
+++ src/Mods/Breaker/DllMain.cpp
@@ -0,0 +1,100 @@
+#include <Windows.h>
+
+#include "Utils.h"
+#include "GameData.h"
+
+using DestroyEffect_t = uint32_t(__cdecl*)(uint32_t paramAddress, int param2, int param3);
+inline DestroyEffect_t DestroyEffect = reinterpret_cast<DestroyEffect_t>(0x00585c10);
+
+Utils::Hook hBreaker;
+
+void Start();
+
+uint32_t __cdecl Effect(uint32_t paramAddress, int param2, int param3);
+bool __cdecl Condition(uint32_t paramAddress, int param2, int param3);
+void StatBuff();
+int __stdcall CheckFlag(uint32_t playerIdx, uint32_t zoneIdx);
+
+DWORD WINAPI MainThread(LPVOID lpParam)
+{
+    Sleep(500);
+    Start();
+
+    return 0;
+}
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hinst);
+        CreateThread(0, 0, &MainThread, 0, 0, NULL);
+    }
+
+    return TRUE;
+}
+void Start()
+{
+    hBreaker = Utils::InstallHook((void*)0x0056dde9, 5, (void*)StatBuff);
+
+    GameData::EffectScript script;
+    script.CardID = 0x96;
+    script.Effect = reinterpret_cast<uintptr_t>(&Effect);
+    script.AppliesTo = 0x0057B4A0;
+    script.Condition = reinterpret_cast<uintptr_t>(&Condition);
+    script.Cost = 0;
+    script.Target = 0x005959D0;
+
+    int idx = GameData::GetEffectScriptIndex(0x96);
+    GameData::SetEffectScript(idx, script);
+
+}
+uint32_t __cdecl Effect(uint32_t paramAddress, int param2, int param3)
+{
+    // Original in-game function
+	uint32_t result = DestroyEffect(paramAddress, param2, param3);
+
+    // Set flag
+    GameData::Player player = GameData::GetDuel().players[1];
+    int zoneIdx = (Utils::ReadUint8((void*)(paramAddress + 0x2)) >> 1) & 0xF;
+    Utils::WriteUint16((void*)(0x00a56aa8 + 0x10 + 0x90 * zoneIdx + 0x48), 0x1);
+
+	return result;
+}
+bool __cdecl Condition(uint32_t paramAddress, int param2, int param3)
+{
+    GameData::Player player = GameData::GetDuel().players[1];
+    int zoneIdx = (Utils::ReadUint8((void*)(paramAddress + 0x2)) >> 1) & 0xF;
+
+    if (Utils::ReadUint8((void*)(0x00a56aa8 + 0x10 + 0x90 * zoneIdx + 0x48)) == 0x1) return false;
+ 
+    return true;
+}
+__declspec(naked) void StatBuff()
+{
+	__asm
+	{
+	hook:
+		CMP EAX, 0x96
+		JNE hook_end
+
+        PUSH EAX
+        PUSH EDI
+        PUSH DWORD PTR DS:[ESP+68]
+        CALL OFFSET CheckFlag
+        CMP EAX, 0x1
+        POP EAX
+        JNE hook_end
+
+        MOV EAX, -300
+        MOV dword ptr[ESP + 0x14], EAX
+        MOV EAX, 0x0056df89
+        JMP EAX
+	hook_end :
+		JMP[hBreaker.Trampoline]
+	}
+}
+int __stdcall CheckFlag(uint32_t playerIdx, uint32_t zoneIdx)
+{
+    if (Utils::ReadUint8((void*)(0x00a55d64 + 0xD44 * playerIdx + 0x10 + 0x90 * zoneIdx + 0x48)) == 0x1) return 1;
+    else return 0;
+}

f00a770fc08810bc08f7b517a33bda04fa4f3285
bitemydusto <bitemydusto@users.noreply.github.com>
2026-08-03 08:44:35 +0200

    Added more in-game functions

--- src/Mods/FUN.h
+++ src/Mods/FUN.h
@@ -10,17 +10,41 @@ namespace FUN
         return reinterpret_cast<T>(address);
     }
 
-    using GetMonsterType_t = uint32_t(__cdecl*)(uint16_t);
+    using GetMonsterType_t = uint32_t(__cdecl*)(uint16_t intID);
     inline GetMonsterType_t GetMonsterType = reinterpret_cast<GetMonsterType_t>(0x004025D0);
 
-    using GetMonsterAttribute_t = uint32_t(__cdecl*)(uint16_t);
+	using GetCardSubType_t = uint32_t(__cdecl*)(uint16_t intID);
+	inline GetCardSubType_t GetCardSubType = reinterpret_cast<GetCardSubType_t>(0x00402740);
+
+    using GetMonsterAttribute_t = uint32_t(__cdecl*)(uint16_t intID);
     inline GetMonsterAttribute_t GetMonsterAttribute = reinterpret_cast<GetMonsterAttribute_t>(0x00402650);
+
+	using GetMonsterLevel_t = uint32_t(__cdecl*)(uint16_t intID);
+	inline GetMonsterLevel_t GetMonsterLevel = reinterpret_cast<GetMonsterLevel_t>(0x004026c0);
+
+	using GetMonsterATK_t = uint32_t(__cdecl*)(uint16_t intID);
+	inline GetMonsterATK_t GetMonsterATK = reinterpret_cast<GetMonsterATK_t>(0x004027b0);
+
+	using GetMonsterDEF_t = uint32_t(__cdecl*)(uint16_t intID);
+	inline GetMonsterDEF_t GetMonsterDEF = reinterpret_cast<GetMonsterDEF_t>(0x00402800);
+
+	using GetCardID_t = uint32_t(__cdecl*)(uint32_t intID);
+	inline GetCardID_t GetCardID = reinterpret_cast<GetCardID_t>(0x004022e0);
+
+	using GetSpellTrapType_t = uint32_t(__cdecl*)(uint16_t intID);
+	inline GetSpellTrapType_t GetSpellTrapType = reinterpret_cast<GetSpellTrapType_t>(0x00402700);
+
+	using GetSpellSpeed_t = uint32_t(__cdecl*)(uint16_t intID);
+	inline GetSpellSpeed_t GetSpellSpeed = reinterpret_cast<GetSpellSpeed_t>(0x0057e030);
 
     using ShowDialogue_t = void(__cdecl*)(const char*);
     inline ShowDialogue_t ShowDialogue = reinterpret_cast<ShowDialogue_t>(0x005bf860);
 
 	using PayLifePoints_t = void(__cdecl*)(unsigned int playerIdx, unsigned int amount);
     inline PayLifePoints_t PayLifePoints = reinterpret_cast<PayLifePoints_t>(0x005783b0);
+
+	using IsCardOnTheField_t = uint32_t(__cdecl*)(uint16_t cardID);
+	inline IsCardOnTheField_t IsCardOnTheField = reinterpret_cast<IsCardOnTheField_t>(0x005699c0);
 
 	// Generates target parameters and saves them to the target pointer
 	using GetTargetParams_t = uint32_t(__cdecl*)(unsigned int block, int param2, unsigned int* target);
